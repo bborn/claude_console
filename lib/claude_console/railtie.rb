c2a8f40574edc0fb06e86a356ca5059172eec28f
Mike Porter <mike@agileedge.com>
2026-02-27 11:50:21 -0800

    Rename command.rb to irb_command.rb for symmetry with pry_command.rb

--- lib/claude_console/railtie.rb
+++ lib/claude_console/railtie.rb
@@ -9,7 +9,7 @@ module ClaudeConsole
       else
         require "irb"
         require "irb/command"
-        require "claude_console/command"
+        require "claude_console/irb_command"
         IRB::Command.register(:claude, ClaudeConsole::Command)
       end
     end
